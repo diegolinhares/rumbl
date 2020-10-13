6d5ff13a1145a473c811cdbf83ff599ade1be415
dlinhares <dlinhares@arbnco.com>
2020-10-12 22:24:53 -0300

    Alterando [Schema] User

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,5 +1,15 @@
 defmodule Rumbl.Accounts.User do
-  @required_keys [:id, :name, :username]
+  use Ecto.Schema
 
-  defstruct @required_keys
+  import Ecto.Changeset
+
+  # O schema e o field se referenciam aos dados da tabela e de uma struct.
+  # Cada field corresponde a um field na struct "gerada".
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
 end
