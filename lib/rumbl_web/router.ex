466bec1672d03a434edf23fce58b241df85fb163
dlinhares <dlinhares@arbnco.com>
2020-10-17 01:27:37 -0300

    Adicionando [Form] para criar Users

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -16,7 +16,7 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
-    get "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/users", UserController, only: [:index, :show, :new, :create]
     get "/", PageController, :index
   end
 
